a76007ecb108d6099948b625346f2fa4af365b2f
adamkozuch <adam.kozuch@gmail.com>
2017-02-09 15:13:55 +0900

    initial script

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+git config --global core.editor vim &&
+sudo apt-get install ruby ruby-dev vim-nox &&
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &&
+cd ~/.vim &&
+ git clone https://github.com/wincent/command-t.git bundle/command-t &&
+cd ~/.vim/bundle/command-t/ruby/command-ti &&
+ruby extconf.rb &&
+make 
+
