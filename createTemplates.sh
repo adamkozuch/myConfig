8aedbe127a6e4c36179d1e15c57cde5d33a877d6
adamkozuch <adam.kozuch@gmail.com>
2017-03-27 13:48:51 +0900

    config

---
+++ createTemplates.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+./monaca-cli/bin/monaca create vueNavigator --url https://github.com/monaca-templates/onsenui-v2-vue-navigation/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create vueMinimum --url https://github.com/monaca-templates/onsenui-v2-vue-minimum/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create vueTabbar --url https://github.com/monaca-templates/onsenui-v2-vue-tabbar/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create vueSplitter --url https://github.com/monaca-templates/onsenui-v2-vue-splitter/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create reactNavigator --url https://github.com/monaca-templates/onsenui-v2-react-navigation/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create reactMinimum --url https://github.com/monaca-templates/onsenui-v2-react-minimum/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create reactTabbar --url https://github.com/monaca-templates/onsenui-v2-react-tabbar/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create reactSplitter --url https://github.com/monaca-templates/onsenui-v2-react-splitter/archive/2.2.7.zip && 
+./monaca-cli/bin/monaca create angNavigator --url https://github.com/monaca-templates/onsenui-v2-angular2-navigation/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create angMinimum --url https://github.com/monaca-templates/onsenui-v2-angular2-minimum/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create angTabbar --url https://github.com/monaca-templates/onsenui-v2-angular2-tabbar/archive/2.2.7.zip &&
+./monaca-cli/bin/monaca create angSplitter --url https://github.com/monaca-templates/onsenui-v2-angular2-splitter/archive/2.2.7.zip 
