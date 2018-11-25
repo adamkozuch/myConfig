c9f744f4a5e7afe3072c93e193ec679a822c88fe
Adam <adam.kozuch@gmail.com>
2018-11-25 20:21:09 +0100

    Create sss.sh

---
+++ sss.sh
@@ -0,0 +1,14 @@
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install neovim
+
+brew install fzf
+
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+
+brew install the_silver_searcher
+
+python -m pip install flake8
+
